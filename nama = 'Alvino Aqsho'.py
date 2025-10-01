a96ba82a36a234166b44c21e6f6edfa9f9c44591
alvinoaqsho15-dev <alvinoaqsho15@gmail.com>
2025-10-01 10:52:17 +0700

    Add files via upload

---
+++ nama = 'Alvino Aqsho'.py
@@ -0,0 +1,9 @@
+saya_orang_indonesia = True
+saya_adalah_robot = False
+ print ('apakah saya orang indonesia?'saya_orang_indonesia)
+print ('apakah saya adalah robot?', saya_adalah_robot)
+print ('tipe dari saya_orang_indonesia', type (saya_orang_indonesia))
+print ('tipe dari saya_adalah_robot', type (saya_adalah_robot))
+
+
+
